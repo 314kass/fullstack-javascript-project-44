7b22a66b496338d8650bf717ef6cc5ebca6ef9e0
Sergey <buuakaka@gmail.com>
2024-01-14 12:57:12 +0300

    fix is-prime

--- bin/is-prime.js
+++ bin/is-prime.js
@@ -1,6 +1,8 @@
-export default (num) => {
-  for (let i = 2; i < num; i++) {
+const isPrime = (num) => {
+  for (let i = 2; i < num; i += 1) {
     if (num % i === 0) return 'no';
   }
   return 'yes';
-}
+};
+
+export default isPrime;
