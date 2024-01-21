6e0c4b5efce79e996010f63ae1dd4bfce1db4f53
Sergey <buuakaka@gmail.com>
2024-01-21 09:58:47 +0300

    brain-calc refactoring

--- bin/games/brain-calc.js
+++ bin/games/brain-calc.js
@@ -5,19 +5,13 @@ import engine from '../../src/index.js';
 const gameDescription = 'What is the result of the expression?';
 function roundGen() {
   const roundGeneration = [];
-  const operator = getRandomInRange(1, 3);
+  const operator = getRandomInRange(0, 2);
+  const operatorChar = ['-', '*', '+']
   const firstNum = getRandomInRange();
   const secondNum = getRandomInRange();
-  if (operator === 1) {
-    roundGeneration.push(`${firstNum} - ${secondNum}`);
-    roundGeneration.push(`${firstNum - secondNum}`);
-  } else if (operator === 2) {
-    roundGeneration.push(`${firstNum} * ${secondNum}`);
-    roundGeneration.push(`${firstNum * secondNum}`);
-  } else {
-    roundGeneration.push(`${firstNum} + ${secondNum}`);
-    roundGeneration.push(`${firstNum + secondNum}`);
-  }
+  roundGeneration.push(`${firstNum} ${operatorChar[operator]} ${secondNum}`);
+  roundGeneration.push(`${eval(`${firstNum} ${operatorChar[operator]} ${secondNum}`)}`);
+  
   return roundGeneration;
 }
 
