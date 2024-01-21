a19f6eee78c6ac42ebddbf26b7681f0995200592
Sergey <buuakaka@gmail.com>
2024-01-21 10:33:39 +0300

    minor change

--- bin/games/brain-prime.js
+++ bin/games/brain-prime.js
@@ -4,7 +4,8 @@ import engine from '../../src/index.js';
 
 const isPrime = (num) => {
   for (let i = 2; i < num; i += 1) {
-    if (num % i === 0) return 'no';
+    if (num % i === 0)
+    return 'no';
   }
   return 'yes';
 };
