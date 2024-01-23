9f1cefe4c2904c9edb2c8c53c0c059f749d9b8d3
Sergey <buuakaka@gmail.com>
2024-01-23 15:43:48 +0300

    update brin-progression

--- bin/games/brain-progression.js
+++ bin/games/brain-progression.js
@@ -3,21 +3,28 @@ import getRandomInRange from '../../src/getRandomInRange.js';
 import engine from '../../src/index.js';
 
 const gameDescription = 'What number is missing in the progression?';
-function roundGen() {
-  const randomNum = getRandomInRange();
-  const plus = getRandomInRange();
-  const quest = [];
-  const roundGeneration = [];
-  for (let i = 0; i < 10; i += 1) {
-    quest.push(`${randomNum + (plus * i)}`);
-  }
-  const whichNum = getRandomInRange(0, 9);
-  const temp = quest[whichNum];
-  quest[whichNum] = '..';
-  roundGeneration.push(quest.join(' '));
-  roundGeneration.push(temp);
 
-  return roundGeneration;
+const correctAnswer = (questGen) => {
+  const whichNum = getRandomInRange(0,9);
+  const quest = questGen;
+  const answer = quest[whichNum];
+  quest[whichNum] = '..';
+  return [quest.join(' '), answer ];
+};
+
+const questGen = (num, plus) => {
+  const questGen = [];
+  for (let i = 0; i < 10; i += 1) {
+    questGen.push(`${num + (plus * i)}`);
+  };
+
+  return correctAnswer(questGen);
+};
+const generateRound = () => {
+  const num = getRandomInRange();
+  const plus = getRandomInRange();
+  const [quest, correctAnswer] = questGen(num, plus);
+  return [quest, correctAnswer];
 }
 
-engine(gameDescription, roundGen);
+engine(gameDescription, generateRound);
