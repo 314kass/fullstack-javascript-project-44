c4d102ccbba12fee2897f5a16d0be4b4fe8c1d0f
Sergey <buuakaka@gmail.com>
2024-01-23 15:50:03 +0300

    fix brain-even

--- bin/games/brain-progression.js
+++ bin/games/brain-progression.js
@@ -5,26 +5,26 @@ import engine from '../../src/index.js';
 const gameDescription = 'What number is missing in the progression?';
 
 const correctAnswer = (questGen) => {
-  const whichNum = getRandomInRange(0,9);
+  const whichNum = getRandomInRange(0, 9);
   const quest = questGen;
   const answer = quest[whichNum];
   quest[whichNum] = '..';
-  return [quest.join(' '), answer ];
+  return [quest.join(' '), answer];
 };
 
-const questGen = (num, plus) => {
+const questGeneration = (num, plus) => {
   const questGen = [];
   for (let i = 0; i < 10; i += 1) {
     questGen.push(`${num + (plus * i)}`);
-  };
+  }
 
   return correctAnswer(questGen);
 };
 const generateRound = () => {
   const num = getRandomInRange();
   const plus = getRandomInRange();
-  const [quest, correctAnswer] = questGen(num, plus);
-  return [quest, correctAnswer];
-}
+  const [quest, answer] = questGeneration(num, plus);
+  return [quest, answer];
+};
 
 engine(gameDescription, generateRound);
