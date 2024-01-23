69c528961935a059b797490cddd51ac14f18dd8a
Sergey <buuakaka@gmail.com>
2024-01-23 16:55:52 +0300

    path fix

--- bin/games/brain-prime.js
+++ bin/games/brain-prime.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import getRandomInRange from '../getRandomInRange.js';
-import engine from '../index.js';
+import getRandomInRange from '../../src/getRandomInRange.js';
+import engine from '../../src/index.js';
 
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
